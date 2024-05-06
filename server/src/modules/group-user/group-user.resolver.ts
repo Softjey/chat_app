10f80029849c44a9b2e0ed5b6f843ef3cf1fea83
Softjey <misivsvatoslav@gmail.com>
2024-05-06 14:36:58 +0200

    add method to get user groups

--- server/src/modules/group-user/group-user.resolver.ts
+++ server/src/modules/group-user/group-user.resolver.ts
@@ -1,4 +1,4 @@
-import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { GroupUser } from './entities/group-user.entity';
 import { UsersService } from 'src/modules/users/users.service';
 import { User } from '../users/entities/user.entity';
@@ -6,14 +6,25 @@ import { GroupsService } from '../groups/groups.service';
 import { Group } from '../groups/entities/group.entity';
 import { MessagesService } from '../messages/messages.service';
 import { Message } from '../messages/entities/message.entity';
+import { GroupUserService } from './group-user.service';
+import { ReqUser } from '../auth/decorators/req-user.decorator';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/gurads/auth.guard';
 
+@UseGuards(AuthGuard)
 @Resolver(() => GroupUser)
 export class GroupUserResolver {
   constructor(
     private readonly usersService: UsersService,
     private readonly groupsService: GroupsService,
     private readonly messagesService: MessagesService,
+    private readonly groupUserService: GroupUserService,
   ) {}
+
+  @Query(() => [GroupUser])
+  getMyGroups(@ReqUser() user: User): Promise<GroupUser[]> {
+    return this.groupUserService.getByUserId(user.id);
+  }
 
   @ResolveField()
   async user(@Parent() groupUser: GroupUser): Promise<User | null> {
