534a521f671ca3534351a6ea69d0fb7adfc113e9
Softjey <misivsvatoslav@gmail.com>
2024-05-09 20:58:08 +0200

    fix gql context

--- server/src/modules/graphql/graphql.module.ts
+++ server/src/modules/graphql/graphql.module.ts
@@ -1,12 +1,14 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule as NestGraphQLModule } from '@nestjs/graphql';
+import type { Request, Response } from 'express';
 
 @Module({
   imports: [
     NestGraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
+      context: ({ req, res }: { req: Request; res: Response }) => ({ req, res }),
     }),
   ],
 })
